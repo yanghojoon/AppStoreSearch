2c36018d57fa80f9e7e83404cdba10a25f097b3d
yanghojoon <yanghojoon8487@gmail.com>
2022-06-02 19:36:01 +0900

    feat: 스토리보드 없이 화면을 띄울 수 있도록 구현
    - Info.plist 수정
    - 프로젝트 TARGETS general 설정 변경
    - SceneDelegate의 scene 메서드에서 rootViewController 설정

---
+++ AppStoreClone/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  AppStoreClone
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        
+        let navigationController = UINavigationController()
+        let appListViewController = AppListViewController()
+        
+        window?.rootViewController = appListViewController
+        appListViewController.view.backgroundColor = .systemBackground
+    }
+    
+}
+
---
+++ AppStoreClone/Presentation/AppList/AppListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+
+import UIKit
+
+class AppListViewController: UIViewController {
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
